d911ed864362a4702d41a8b0d83851f236339faf
trucane <trufashionista2013@gmail.com>
2020-04-24 08:18:48 -0600

    connected to mongodb server with mongoose, added db and port to .env

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+require('dotenv/config')
+
+
+mongoose.connect(process.env.DB_PASSWORD,
+    {useNewUrlParser:true, useUnifiedTopology:true},
+    ()=> console.log('connected by mongoose')
+)
+
 
 
 app.get('/', (req, res) => {
-    res.send('<h1> We are working </h1>')
+    res.send('<h1> We are working with </h1>')
 });
 
-app.listen(3000, ()=> console.log('reporting console from server'))
+app.listen(process.env.PORT, ()=> console.log('reporting console from server'))
